8e7a5e7eeb0fde0692b138305dd8a03465a4a6d9
Andy <andyjrosas@gmail.com>
2019-04-26 19:49:40 -0700

    minor update

--- src/RootFinder.java
+++ src/RootFinder.java
@@ -6,6 +6,7 @@ public class RootFinder {
 
     public void bisection(float p1, float p2, int maxIter, float epsilon, String fileName) {
         Polynomial poly = new Polynomial(fileName);
-        poly.print();
+
+
     }
 }
